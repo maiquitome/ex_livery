bea29d1f5d01194c483dc507b2b016037b0141f4
Maiqui Pirolli Tomé <maiquitome@gmail.com>
2021-05-12 10:31:32 -0300

    :white_check_mark: add item test

---
+++ test/orders/item_test.exs
@@ -0,0 +1,43 @@
+defmodule ExLivery.Orders.ItemTest do
+  use ExUnit.Case
+
+  alias ExLivery.Orders.Item
+
+  import ExLivery.Factory
+
+  describe "build/4" do
+    test "when all params are valid, returns an item" do
+      response = Item.build("Pizza de peperoni", :pizza, "35.5", 1)
+
+      expected_response = {:ok, build(:item)}
+
+      assert response == expected_response
+    end
+
+    test "when there is an invalid category, returns an error" do
+      response = Item.build("Pizza de peperoni", :banana, "35.5", 1)
+
+      expected_response =
+        {:error,
+         "Category must be one of these: :pizza :hamburguer :carne :prato_feito :japonesa :sobremesa"}
+
+      assert response == expected_response
+    end
+
+    test "when there is an invalid price, returns an error" do
+      response = Item.build("Pizza de peperoni", :pizza, "price", 1)
+
+      expected_response = {:error, "invalid price!"}
+
+      assert response == expected_response
+    end
+
+    test "when there is an invalid quantity, returns an error" do
+      response = Item.build("Pizza de peperoni", :pizza, "35.5", 0)
+
+      expected_response = {:error, "Quantity must have a value greater than 0!"}
+
+      assert response == expected_response
+    end
+  end
+end
